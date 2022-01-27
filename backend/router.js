ab931245501a92f018d453413b2cd3bfb41f6029
mehan <mehan.alavi.88.majd@gmail.com>
2022-01-27 15:49:40 +0330

    Added backend

--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+var mysql = require('mysql'); 
+app.use(bodyParser.urlencoded({ extended: true }));
 let dirname;
 // code to get the current directory and delete /backend at end from it
 dirname = __dirname.substring(0, __dirname.length - 7);
@@ -11,7 +14,14 @@ app.get("/signup", (req, res) => {
     // render the html file
     res.sendFile(dirname + "/html/signup/signup.html");
 });
+app.post("/signup", (req, res) => {
+    // get the form
+    let form = req.body;
+    // get the form data
+    console.log(form);
+});
+
 // listen on port 8080
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+app.listen(4000, () => {
+    console.log("Server started on port 4000");
 });
