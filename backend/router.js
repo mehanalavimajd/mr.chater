fa91d6f7fda93c7cb7030de2a34f708b247728d1
mehan <mehan.alavi.88.majd@gmail.com>
2022-02-10 11:52:28 +0330

    Some changes

--- backend/router.js
+++ backend/router.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-var mysql = require('mysql'); 
+const mysql = require("mysql");
+const session = require("express-session");
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password", // really secure XD
+  database: "project",
+});
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 let dirname;
 // code to get the current directory and delete /backend at end from it
@@ -11,17 +27,39 @@ app.use(express.static(dirname + "/public"));
 
 // gets / and render "../html/signup.html"
 app.get("/signup", (req, res) => {
-    // render the html file
-    res.sendFile(dirname + "/html/signup/signup.html");
+  // render the html file
+  res.sendFile(dirname + "/html/signup.html");
 });
 app.post("/signup", (req, res) => {
-    // get the form
-    let form = req.body;
-    // get the form data
-    console.log(form);
+  // get the form
+  let form = req.body;
+  let pass = form.password;
+  let name = form.username;
+  // create users table if it doesn't exist
+  con.query(
+    "CREATE TABLE IF NOT EXISTS users ( username VARCHAR(255), password VARCHAR(255))",
+    function (err, result) {
+      if (err) throw err;
+    }
+  );
+  // adding name and pass to the database (Not throwing error for non-unique username)
+  con.query(
+    "INSERT INTO users (username, password) VALUES ('" +
+      name +
+      "', '" +
+      pass +
+      "')",
+    function (err, result) {
+      if (err) {
+        throw err;
+      } else {
+        console.log("1 record inserted");
+      }
+    }
+  );
 });
 
 // listen on port 8080
-app.listen(4000, () => {
-    console.log("Server started on port 4000");
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
 });
